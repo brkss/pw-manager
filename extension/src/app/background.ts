0f49edc4db77c9eb84f028b94c9ce7bb37ed644f
Brahim Berkasse <berkassebrahim@gmail.com>
2022-01-08 20:40:55 +0100

    🔦 get tab URL

--- extension/src/app/background.ts
+++ extension/src/app/background.ts
@@ -1,13 +1,9 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("Background got a message!");
-  console.log("message", message);
-  console.log("sender : ", sender);
+
+  const url: string = sender.url;
+  console.log("URL => ", url);
+  // send url to api
 
   sendResponse({});
-});
-
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-  let url = tabs[0].url;
-  // use `url` here inside the callback because it's asynchronous!
-  console.log("url -> ", url);
 });
--- extension/src/app/content.ts
+++ extension/src/app/content.ts
@@ -10,9 +10,3 @@ chrome.runtime.sendMessage({}, (response) => {
     }
   });
 });
-
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-  let url = tabs[0].url;
-  console.log("url =>>", url);
-  // use `url` here inside the callback because it's asynchronous!
-});
